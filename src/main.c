22eff4002bc2b4cef79b5c402c6e0a1c04e5fe73
HuevoChorizo <jjimenezvinuesa@gmail.com>
2025-12-06 20:20:23 +0100

    He currao?

--- src/main.c
+++ src/main.c
@@ -18,12 +18,16 @@
  * THIS FILE IS TO BE MODIFIED
  */
 
+#include <signal.h>
 #include <stddef.h> /* NULL */
 #include <stdio.h>  /* setbuf, printf */
 #include <stdlib.h>
 #include <string.h>/*strcmp, strlen,...*/
+#include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
 #define ansiblue "\033[1;36m"
 #define ansigreen "\x1b[1;32m"
 #define ansireset "\x1b[0m"
@@ -50,16 +54,46 @@ int mascara(char *entrada) {
     umask(mascAnt);
     return mascAnt;
   }
+
   int octal;
   octal = strtol(entrada, NULL, 8);
   mode_t mascAnt = umask(octal);
   return mascAnt;
 }
+int limites() {
+  struct rlimit *limite = malloc(2 * sizeof(rlim_t));
+  getrlimit(RLIMIT_CPU, limite);
+  fprintf(stdout, "cpu\t%ld\n", limite->rlim_max);
+  getrlimit(RLIMIT_FSIZE, limite);
+  fprintf(stdout, "fsize\t%ld\n", limite->rlim_max);
+  getrlimit(RLIMIT_DATA, limite);
+  fprintf(stdout, "data\t%ld\n", limite->rlim_max);
+  getrlimit(RLIMIT_STACK, limite);
+  fprintf(stdout, "stack\t%ld\n", limite->rlim_max);
+  getrlimit(RLIMIT_CORE, limite);
+  fprintf(stdout, "core\t%ld\n", limite->rlim_max);
+  getrlimit(RLIMIT_NOFILE, limite);
+  fprintf(stdout, "nofile\t%ld\n", limite->rlim_max);
+  free(limite);
+  return 0;
+}
 
-int set();
+int limit(int recurso, char *argv, char *entrada) {
+  struct rlimit *limite = malloc(2 * sizeof(rlim_t));
+  if (entrada == NULL) {
+    getrlimit(recurso, limite);
+    fprintf(stdout, "%s\t%ld\n", argv, limite->rlim_max);
+  } else {
+    int lmt = strtol(entrada, NULL, 8);
+    /*TODO: Comprobar que si limit = -1, se establece el máximo.*/
+    getrlimit(recurso, limite);
+    limite->rlim_max = lmt;
+    setrlimit(recurso, limite);
+  }
+  return 0;
+}
 
-/*TODO: método switch (o switch dentro del main) para seleccionar los otros
- * métodos*/
+int set(char *variable, );
 
 int main(void) {
   fprintf(stderr, "\n");
@@ -79,7 +113,7 @@ int main(void) {
     char cwd[256];
     getcwd(cwd, sizeof(cwd));
     fprintf(stderr, ansiblue "%s\n", cwd);
-    fprintf(stderr, ansigreen "☭ > " ansireset);
+    fprintf(stderr, ansigreen "msh ☭ > " ansireset);
 
     ret = obtain_order(&argvv, filev, &bg);
     if (ret == 0)
@@ -91,13 +125,6 @@ int main(void) {
       continue; /* Empty line */
 #if 1
 
-    /*TODO: el cd*/
-
-    /*
-     * LAS LINEAS QUE A CONTINUACION SE PRESENTAN SON SOLO
-     * PARA DAR UNA IDEA DE COMO UTILIZAR LAS ESTRUCTURAS
-     * argvv Y filev. ESTAS LINEAS DEBERAN SER ELIMINADAS.
-     */
     for (argvc = 0; (argv = argvv[argvc]); argvc++) {
       if (strcmp(argv[0], "cd") == 0) {
         int a = cd(argv[1]);
@@ -106,22 +133,52 @@ int main(void) {
       } else if (strcmp(argv[0], "umask") == 0) {
         int a = mascara(argv[1]);
         printf("%o\n", a);
+      } else if (strcmp(argv[0], "set") == 0) {
+        for (argc = 0; argv[argc]; argc++) {
+          // printf("%s ", argv[argc]);
+        }
+      } else if (strcmp(argv[0], "limit") == 0) {
+        if (argv[1] == NULL)
+          limites();
+        else if (strcmp(argv[1], "cpu") == 0) {
+          limit(RLIMIT_CPU, argv[1], argv[2]);
+        } else if (strcmp(argv[1], "fsize") == 0) {
+          limit(RLIMIT_FSIZE, argv[1], argv[2]);
+        } else if (strcmp(argv[1], "data") == 0) {
+          limit(RLIMIT_DATA, argv[1], argv[2]);
+        } else if (strcmp(argv[1], "stack") == 0) {
+          limit(RLIMIT_STACK, argv[1], argv[2]);
+        } else if (strcmp(argv[1], "core") == 0) {
+          limit(RLIMIT_CORE, argv[1], argv[2]);
+        } else if (strcmp(argv[1], "nofile") == 0) {
+          limit(RLIMIT_NOFILE, argv[1], argv[2]);
+        } else {
+          fprintf(
+              stderr,
+              "El recurso: «%s» no existe o no está implementada su gestión\n",
+              argv[1]);
+        }
+
+      } else if (strcmp(argv[0], "set") == 0) {
+        set();
+      } else {
+        pid_t pid = fork();
+
+        if (pid == -1) {
+          fprintf(stderr, "ERROR fork");
+        } else if (pid == 0) {
+          if (execvp(argv[0], argv) == -1) {
+            fprintf(stderr, "ERROR execvp");
+          }
+        } else {
+          int valor;
+          wait(&valor);
+        }
       }
-      for (argc = 0; argv[argc]; argc++) {
-        // printf("%s ", argv[argc]);
-      }
+
       printf("\n");
     }
   }
-  if (filev[1])
-    printf("> %s\n", filev[1]); /* OUT */
-  if (filev[2])
-    printf(">& %s\n", filev[2]); /* ERR */
-  if (bg)
-    printf("&\n");
-/*
- * FIN DE LA PARTE A ELIMINAR
- */
 #endif
   exit(0);
   return 0;
